44a30110e37fe2dba69f485d92229f81f9e87adf
rgkip01 <web.nogs@gmail.com>
2018-06-19 22:03:23 -0300

    feat(métodos adicionados)

--- cards.rb
+++ cards.rb
@@ -1,17 +1,9 @@
-def inserir_idioma()
-  puts 'Insira uma expressão em Português:'
-  portugues = gets.chomp
-  puts 'Insira uma expressão em Ingles:'
-  ingles = gets.chomp
-  card = { portugues: portugues,  ingles: ingles }
-  return card
-end
+INSERIR = 1
+IMPRIMIR = 2
+BUSCAR = 3
+SAIR = 4
 
-def imprime_cards(cartao)
-  puts "Card =>> #{cartao[:portugues]} | Card =>> #{cartao[:ingles]} encontrado" 
-end
-
-def menu()
+def menu
     puts "Digite a opção desejada: "
     puts
     puts "[1] Inserir um novo card"
@@ -22,39 +14,67 @@ def menu()
     print 'Escolha uma opção: '
     return gets.to_i  
 end
-card = {}
+
+def inserir_idiomas
+  puts 'Insira uma expressão em Português:'
+  portugues = gets.chomp
+  puts 'Insira uma expressão em Inglês:'
+  ingles = gets.chomp
+  card = { portugues: portugues,  ingles: ingles }
+  return card
+end
+
+def cards_inseridos(cards)
+    puts "Aqui estão todos os cards inseridos no sistema:"
+    puts "Português: #{cards[:portugues]} || Inglês: #{cards[:ingles]}"
+end
+
+def imprime_cards(cartao)
+  puts "Card em Português =>> #{cartao[:portugues]} | Card em Inglês =>> #{cartao[:ingles]}" 
+end
+
+def filtrar_idioma
+  puts "Em qual idioma deseja procurar?"
+  puts "[p] Português" 
+  puts "[i] Inglês"
+  buscar_idioma = gets.chomp.upcase
+  return buscar_idioma
+end
+
+def verificar_texto
+  print "digite seu card:"
+  texto_busca = gets.chomp
+  return texto_busca
+end
+
 cards = []
 
 puts 'Bem vindo ao Cards System'
 puts
-opcao = menu()
+opcao = menu
 
-while opcao != 4
-  if opcao == 1
-    inserir_idioma()
-     puts "Cards inseridos: #{card[:portugues]} ==> #{card[:ingles]}"
+while opcao != SAIR
+  if opcao == INSERIR
+    c = inserir_idiomas
+     cards_inseridos(c)
+    cards << c 
 
-  elsif opcao == 2
-    puts "Aqui estão todos os cards cadastrados: "
-    puts cards
+  elsif opcao == IMPRIMIR
+    cards.each do |card|
+     imprime_cards(card)
+    end
 
-  elsif opcao == 3
-    puts  "Em qual idioma deseja procurar?"
-    puts "[p] Português" 
-    puts "[i] Inglês"
-    buscar_idioma = gets.chomp.upcase
-
-    print "digite seu card: #{} "
-    texto_busca = gets.chomp
-
+  elsif opcao == BUSCAR
+     filtrar = filtrar_idioma
+     verificar = verificar_texto
 
     cards.each do |c|
-      if buscar_idioma == "P"
-        if c[:portugues].include? texto_busca
+      if filtrar == "P"
+        if c[:portugues].include?(verificar)
          imprime_cards(c)
         end
-      elsif 
-        if c[:ingles].include? texto_busca
+      elsif filtrar == "I"
+        if c[:ingles].include?(verificar)
           imprime_cards(c)
         end
       else
@@ -66,5 +86,11 @@ while opcao != 4
     puts 'entre com uma opcao válida'
     puts
   end
-  opcao = menu()
+  opcao = menu
 end
+
+
+# criar card como obejto
+# verficar se o card já existe 
+# include só por inglês ou português
+# Garantir que não crie um card duplicado com um comparação  
